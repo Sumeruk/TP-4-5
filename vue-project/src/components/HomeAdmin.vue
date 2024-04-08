75c53f589b2cab38a4ae23cc2a8837f4a84d2682
ElenaDmitrova <100825917+ElenaDmitrova@users.noreply.github.com>
2024-04-09 00:33:28 +0300

    update componentes

---
+++ vue-project/src/components/HomeAdmin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Домашняя страница</h1>
+    <!-- Контент домашней страницы -->
+    <h2>sdfughujkl</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // Логика компонента
+};
+</script>
+
+<style>
+/* Стили компонента */
+</style>
